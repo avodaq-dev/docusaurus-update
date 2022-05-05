dd0b2fd910d9a04e694d7af4c127821c852e7e2b
aersosi <aersosi@avodaq.com>
2022-05-05 13:51:06 +0200

    [init]

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import PixiComp from "@site/src/components/PixiComp";
+
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <PixiComp />
+
+    </Layout>
+  );
+}
